00c3c9e938d60c45f6857e615ae5f7c3b1d7e89c
mirkogalizia <mirko.galizia@outlook.com>
2025-07-26 21:54:29 +0200

    fix: rimuovi analytics e correggi firebase config per Firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 // src/firebase.js
 import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -9,11 +8,10 @@ const firebaseConfig = {
   projectId: "ehilab",
   storageBucket: "ehilab.firebasestorage.app",
   messagingSenderId: "787841555610",
-  appId: "1:787841555610:web:45a261d9412cbe7212d323",
-  measurementId: "G-PFJHQ1XMG1"
+  appId: "1:787841555610:web:45a261d9412cbe7212d323"
+  // ❌ NON serve measurementId o databaseURL per Firestore
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
 export const db = getFirestore(app);
