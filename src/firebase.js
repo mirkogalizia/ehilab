589bc62e700c8dcb4d177c579bdd7b2ff0fd704b
mirkogalizia <mirko.galizia@outlook.com>
2025-07-26 22:13:46 +0200

    fix: rimosso analytics per evitare errore SSR

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // src/firebase.js
 import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -8,10 +9,13 @@ const firebaseConfig = {
   projectId: "ehilab",
   storageBucket: "ehilab.firebasestorage.app",
   messagingSenderId: "787841555610",
-  appId: "1:787841555610:web:45a261d9412cbe7212d323"
-  // ❌ NON serve measurementId o databaseURL per Firestore
+  appId: "1:787841555610:web:45a261d9412cbe7212d323",
+  measurementId: "G-PFJHQ1XMG1"
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
+// const analytics = getAnalytics(app); // ❌ disattivato per ambienti server
+
+export const auth = getAuth(app);
 export const db = getFirestore(app);
