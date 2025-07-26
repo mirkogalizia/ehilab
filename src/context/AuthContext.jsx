1c814f482fede9c6bb1736caa6c2b96a8b2585c6
mirkogalizia <mirko.galizia@outlook.com>
2025-07-26 23:40:39 +0200

    feat: aggiunto AuthProvider nel RootLayout

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,44 @@
+// src/context/AuthContext.jsx
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+
+      if (currentUser) {
+        const docRef = doc(db, "utenti", currentUser.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setUserData(docSnap.data());
+        }
+      } else {
+        setUserData(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, userData, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
