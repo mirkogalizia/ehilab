35f26b78d86bf181670aa44508b1fea5a49fe854
mirkogalizia <mirko.galizia@outlook.com>
2025-07-27 23:46:03 +0200

    ✅ Fix definitivo import firebase con 2 livelli

--- src/app/chatboost/impostazioni/info/page.jsx
+++ src/app/chatboost/impostazioni/info/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { auth, db } from "@/firebase/config";
+import { auth, db } from "../../../firebase";
 import { doc, getDoc } from "firebase/firestore";
 
 export default function InfoPage() {
