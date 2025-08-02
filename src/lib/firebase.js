a2d856bfd7eb0b4f8a8ebf7a5a317cc95363762a
mirkogalizia <mirko.galizia@outlook.com>
2025-08-03 01:14:51 +0200

    Aggiunta gestione

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage'; // <--- AGGIUNGI QUESTA RIGA
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDHkexQNuc09zuisYnkPjht3TLy1rfLO_M',
   authDomain: 'ehilab.firebaseapp.com',
   projectId: 'ehilab',
-  storageBucket: 'ehilab.appspot.com',
+  storageBucket: 'ehilab.appspot.com', // ⚠️ CORRETTO (.appspot.com)
   messagingSenderId: '787841555610',
   appId: '1:787841555610:web:45a261d9412cbe7212d323',
   measurementId: 'G-PFJHQ1XMG1',
@@ -16,5 +17,6 @@ const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);  // <--- AGGIUNGI QUESTA RIGA
 
-export { db, auth };
+export { db, auth, storage };
